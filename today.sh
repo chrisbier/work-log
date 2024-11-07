fcb754d5d76dcc6cb89f512ee39d45274884f487
Chris Bier <chris.bier@imanage.com>
2024-11-07 08:51:42 -0600

    New today.sh to work with Logseq

--- today.sh
+++ today.sh
@@ -1,19 +1,71 @@
 #!/bin/bash
-
 thisyear=$(date +%Y)
 export thisyear
-todayfile=$thisyear/$(date +%m)/$(date +%Y%m%d).md
+todayfile=journals/$(date +%Y_%m_%d).md
 export todayfile
 yesterdaymonth=$(date +%m --date="yesterday")
 export yesterdaymonth
+TODAY=$(date +%Y_%m_%d)
+JOURNALS="journals"
+PAGES="pages"
+
+# Create initial
+cat << EOF > ./${JOURNALS}/${TODAY}.md
+- # 🕬 Interrupts
+    -
+- # 📆 Meetings
+
+- # ✏️ Work
+
+EOF
+
+## Add in Today's Meetings
+
+# Get Today's Meetings Template
+MEETINGS_FILE_PREFIX="${PAGES}/Templates___$(date +%A)s"
+# Is Sprint Week
+weekofyear=$(date +%V)
+POSTFIX=""
+SPRINT_WEEK=true
+MEETINGS_FILE="${MEETINGS_FILE_PREFIX}.md"
+TEMP_FILE="/tmp/meetings.md"
+if [[ -f "${TEMP_FILE}" ]]
+then 
+    rm "${TEMP_FILE}"
+fi
+
+# If week of year is even SPRINT_WEEK is False
+if [[ $((weekofyear % 2)) == 0 ]]
+then
+    SPRINT_WEEK=false
+    POSTFIX="Off-Sprint"
+    # If the file exists, use it, if not, no changes.
+    if [[ -f "${MEETINGS_FILE_PREFIX} ${POSTFIX}.md" ]]
+    then
+        MEETINGS_FILE="${MEETINGS_FILE_PREFIX} ${POSTFIX}.md"
+    fi
+fi
+
+# Extract just the Templated Meetings
+grep -E '^\s+\-' "${MEETINGS_FILE}" > ${TEMP_FILE}
+
+# Insert today's meeting schedule
+sed -ie '/📆 Meetings/r '"${TEMP_FILE}" "${JOURNALS}/${TODAY}.md"
+
+rm "${TEMP_FILE}"
+
+## End Today's Meetings
+
+
+## Copy Yesterday's Work
 
 retval=""
 
 function find_last_log() {
     day_value=$1
     month_value=$(date +%m --date="now - $day_value days")
-    filename=$(date +%Y%m%d --date="now - $day_value days").md
-    filename_full_path=$thisyear/$month_value/$filename 
+    filename=$(date +%Y_%m_%d --date="now - $day_value days").md
+    filename_full_path="${JOURNALS}/${filename}"
 
     if [ "$day_value" -gt 30 ]
     then
@@ -30,34 +82,20 @@ function find_last_log() {
     fi
 }
 
-#yesterdayfile=$(find_last_log 1 )
+# yesterdayfile=$(find_last_log 1 )
 find_last_log 1
 yesterdayfile=$retval
-
-# Initialize File with Typical Meetings
-# TODO have something query Outlook for my calendar and add meetings here
-cat <<EOF >> "$todayfile"
-
-# SRE-Share Standup
-# CPE Standup
-# SRE-Work Standup
-# SRE-Core Standup
-
-EOF
-# if the day file doesn't exist, grep the TODOs from yesterday and copy them over
-#if [ ! -e $todayfile ]
-#then
-#    yesterday=$(date --date="1 day ago" +%Y%m%d".md")
-#    yesterdaymonth=$(date --date="1 day ago" +%m)
-#    yesterdayfile=$thisyear/$yesterdaymonth/$yesterday
-#    yesterdayfile=$thisyear/$yesterdaymonth/$(python3 ./relative_date.py)".md"
+# echo $yesterdayfile
 
 if [ -e "$yesterdayfile" ]
 then
-    grep -i todo "$yesterdayfile" | awk '{ if ($1 ~ /^#/) { print "\n"$0 } else { print $0 } }' >> "$todayfile"
-    echo >> "${todayfile}"
+    # Find Work section and add Yesterday's Work
+    grep -A 200 -E "✏️ Work" ${yesterdayfile} | grep -E "^\s-" | sed '/^\s-/a\            - ' >> ${todayfile}
+else
+    echo "ERROR: Yesterday File ${yesterdayfile} Not Found"
 fi
-#fi
 
-# Open yesterday, today, and any other misc files in gvim with vertical splits
-gvim -O $yesterdayfile $todayfile $1 $2 $3 $4 $5
+## End Copy Work
+
+# Delete temp file
+rm "./${JOURNAL}/${TODAY}.mde"
